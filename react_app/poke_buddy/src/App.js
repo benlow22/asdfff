7ed74023245926bcb6b99704f50f9fc8321a965f
benlow22 <ben2.2low@gmail.com>
2022-11-01 16:45:10 -0700

    Add Pikachu logo to entry page

--- react_app/poke_buddy/src/App.js
+++ react_app/poke_buddy/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import logo from './pokemonLogo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="pokemon_Logo" alt="pokemon Logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to PokeBuddy
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter
         </a>
       </header>
     </div>
