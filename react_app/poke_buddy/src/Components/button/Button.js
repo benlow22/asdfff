c25c67b7a20be430be3663a1b889c63b4de4fe3e
benlow22 <ben2.2low@gmail.com>
2022-11-01 16:09:33 -0700

    Add render and onClick to Button component

---
+++ react_app/poke_buddy/src/Components/button/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export class Button extends React.Component {
+
+    handleClick() {
+        this.props.onClick
+    }
+
+    render(){
+        return (
+            <button 
+                onClick={this.handleClick} 
+                className={this.props.typeName}>
+                {this.props.typeName}
+            </button>
+        )
+    }
+}
