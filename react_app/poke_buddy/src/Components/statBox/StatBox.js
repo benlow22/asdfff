6220cfa3af868cf9efd8efe37a442223f4c1dbd8
benlow22 <ben2.2low@gmail.com>
2022-11-03 17:23:41 -0700

    add StatboxComponent - will hold resistant or weak

---
+++ react_app/poke_buddy/src/Components/statBox/StatBox.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function StatBox (props) {
+    let status = '';
+    return (
+        <div>
+            <h3>`${props.status}`to </h3>
+        </div>
+    )
+}
