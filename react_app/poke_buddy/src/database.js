3ca420ecc5314296d61060361e4fddcd367f9440
benlow22 <ben2.2low@gmail.com>
2022-11-03 17:07:59 -0700

    Add ALL types to keys in database

--- react_app/poke_buddy/src/database.js
+++ react_app/poke_buddy/src/database.js
@@ -1,4 +1,8 @@
 export const pokemonTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'];
 
 
-export const keys = ['Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5'];
+export const keys = [
+    'Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5',
+    'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT',
+    'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 
+    'KeyZ', 'KeyX', 'KeyC' ];
