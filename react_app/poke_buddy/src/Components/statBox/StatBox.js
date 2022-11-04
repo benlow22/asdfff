630332268c5e3b28e5ad95bc096302203f041915
benlow22 <ben2.2low@gmail.com>
2022-11-03 18:14:00 -0700

    adjust formatting, h5 flex box

--- react_app/poke_buddy/src/Components/statBox/StatBox.js
+++ react_app/poke_buddy/src/Components/statBox/StatBox.js
@@ -2,9 +2,17 @@ import React from "react";
 
 export function StatBox (props) {
     let status = props.status;
+    let multipliers = []
+    if (status === 'Weak') {
+        multipliers = ['2.56x', '1.60x'];
+    } if (status === 'Resistant') {
+        multipliers = ['0.391x', '0.625x' ]
+    }
     return (
         <div className="statBox">
-            <h3>{status} to </h3>
+            <h4>{status} to </h4>
+            <h5>{multipliers[0]} damage from</h5>
+            <h5>{multipliers[1]} damage from</h5>
         </div>
     )
 }
